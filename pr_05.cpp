9154efc95c78463aaf31c099b2678850eb6ab4f5
yuriBaza23 <yuribaza2321@gmail.com>
2022-09-10 01:06:01 -0300

    pratica dois: piscar leds sequencialmente r to l (ida e volta)

---
+++ pr_05.cpp
@@ -0,0 +1,88 @@
+// 8 LEDs ligados sequencialmente da direita para a esquerda 1 DE CADA VEZ (IDA E VOLTA)
+
+void turnOffLeds(char id) {
+  if(id == 'D') {
+  	PORTD = 0;
+  } else if(id == 'B') {
+  	PORTB = 0;
+  }
+}
+
+void setup() {
+	DDRB |= (1<<DDB0) | (1<<DDB1) | (1<<DDB2) | 
+      (1<<DDB3) | (1<<DDB4) | (1<<DDB5);  
+	DDRD |= (1<<DDD7) | (1<<DDD6);
+}
+
+void loop()
+{
+  PORTD ^= (1<<DDD6);
+  delay(300);
+  turnOffLeds('D');
+  delay(300);
+  
+  PORTD ^= (1<<DDD7);
+  delay(300);
+  turnOffLeds('D');
+  delay(300);
+  
+  PORTB ^= (1<<DDB0);
+  delay(300);
+  turnOffLeds('B');
+  delay(300);
+  
+  PORTB ^= (1<<DDB1);
+  delay(300);
+  turnOffLeds('B');
+  delay(300);
+  
+  PORTB ^= (1<<DDB2);
+  delay(300);
+  turnOffLeds('B');
+  delay(300);
+  
+  PORTB ^= (1<<DDB3);
+  delay(300);
+  turnOffLeds('B');
+  delay(300);
+  
+  PORTB ^= (1<<DDB4);
+  delay(300);
+  turnOffLeds('B');
+  delay(300);
+  
+  PORTB ^= (1<<DDB5);
+  delay(300);
+  turnOffLeds('B');
+  delay(300);
+  
+  PORTB ^= (1<<DDB4);
+  delay(300);
+  turnOffLeds('B');
+  delay(300);
+  
+  PORTB ^= (1<<DDB3);
+  delay(300);
+  turnOffLeds('B');
+  delay(300);
+  
+  PORTB ^= (1<<DDB2);
+  delay(300);
+  turnOffLeds('B');
+  delay(300);
+  
+  PORTB ^= (1<<DDB1);
+  delay(300);
+  turnOffLeds('B');
+  delay(300);
+  
+  PORTB ^= (1<<DDB0);
+  delay(300);
+  turnOffLeds('B');
+  delay(300);
+  
+  PORTD ^= (1<<DDD7);
+  delay(300);
+  turnOffLeds('D');
+  delay(300);
+}
